0a86ffc7b9e9d99215ce9982556e88368747a573
MochamadAlphin <mochamadalphin853@gmail.com>
2025-05-10 15:41:42 +0700

    Update ChatMessage.kt

--- UTS_Project_MediciPaw/app/src/main/java/com/example/uts_project_medicipaw/ChatMessage.kt
+++ UTS_Project_MediciPaw/app/src/main/java/com/example/uts_project_medicipaw/ChatMessage.kt
@@ -1,8 +1,8 @@
 package com.example.uts_project_medicipaw
 data class ChatMessage(
     val profileImage: Int,
-    val userName: String,      // Pastikan ada properti ini
-    val lastMessage: String,    // Pastikan ada properti ini
+    val userName: String,      
+    val lastMessage: String,    
     val time: String,
-    val category: String        // Jika ada kategori
+    val category: String        
 )
